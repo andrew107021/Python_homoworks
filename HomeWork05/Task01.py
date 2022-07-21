9e584b889639ad5682e8fec3472848b48e45e556
Andrei Beliaev <abbot190414@gmail.com>
2022-07-22 02:47:22 +0300

    Homework 5. Completed

--- HomeWork05/Task01.py
+++ HomeWork05/Task01.py
@@ -6,8 +6,89 @@
 # чтобы забрать все конфеты у своего конкурента?
 # a) Добавьте игру против бота
 # b) (доп) Подумайте как наделить бота ""интеллектом""
+#
+#
+from random import randint
 
-# 2. Создайте программу для игры в "Крестик и -нолики".
+CANDIES_ON_TABLE = 2021
+# CANDIES_ON_TABLE = 121    # testing value
+AMT_MAX = 28
 
-# 3. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
+def two_players():
+    names = ('First', 'Second')
+    person = 0
+    candies = CANDIES_ON_TABLE
+    flag = 1
+
+    print(
+        '''
+        Number of candies one may take is 0 to 28 pieces.
+        You take more then 28 or less then 0 -- You take 28 =). 
+    '''
+    )
+
+    while flag:
+        print(f'Amount of candies: {candies}')
+        num_take = int(input(f'How much candies you want to take (0 min, 28 max),{names[person]}? '))
+        if num_take > 28 or num_take < 0:
+            num_take = AMT_MAX
+        print(f'{names[person]} takes {num_take} candies.')
+        candies -= num_take
+        if candies <=0:
+            flag = 0
+        person = (person + 1) % 2
+
+    print(f'{names[person]} wins')
+
+
+def game_play() -> None:
+    """basic logic"""
+    global names
+    candies = CANDIES_ON_TABLE
+    person = randint(0, 1)
+    flag = 1
+    while flag:
+        print(f'Конфет на столе: {candies}')
+        if person:
+            num_take = human()
+            if num_take > 28 or num_take < 0:
+                num_take = AMT_MAX
+        else:
+            num_take = ai_player(candies)
+        print(f'{names[person]} берёт {num_take} конфет.')
+        candies -= num_take
+        if candies <= 0:
+            print(f'{names[person]} победил')
+            flag = 0
+        person = (person + 1) % 2
+
+
+def ai_player(candies: int) -> int:
+    """primitive AI"""
+    if candies < 28:
+        return candies
+    elif candies - 28 > 1 and candies < 28 * 2:
+        return candies - 27
+    else:
+        return 28
+
+
+def human() -> int:
+    """human capture"""
+    global names
+    return int(input(f'Сколько конфет берёте (0 min, 28 max), {names[1]}? '))
+
+
+print(
+    '''
+    Играем в конфеты. 
+    Полиси: можно взять от 0 до 28 штук.
+    Если взять меньше 0 (интересный вариант...) или больше 28,
+    считается что взял 28. Поехали.    
+'''
+    )
+
+names = ('An artificial one', input('Enter name: '))
+
+game_play()
--- HomeWork05/Task03.py
+++ HomeWork05/Task03.py
@@ -1,2 +1,54 @@
 # 3. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
+
+def rle_mtd(string: str) -> str:
+    """
+    Basic logic for encoding
+    :param string: str
+    :return: string_out: str
+    """
+    string_out = ''
+    count = 1
+    while string:
+        if len(string) > 1:
+            if string[0] == string[1]:
+                count += 1
+                string = string[1:]
+            else:
+                string_out += str(count) + string[0]
+                string = string[1:]
+                count = 1
+        else:
+            string_out += str(count) + string[0]
+            string = ''
+
+    return string_out
+
+
+def rle_bck(string: str) -> str:
+    """
+    Basic logic for decoding
+    :param string: str
+    :return: string_out: str
+    """
+    string_out = ''
+    count = ''
+    while string:
+        if len(string):
+            if string[0].isdigit():
+                count += string[0]
+                string = string[1:]
+            else:
+                count = int(count)
+                string_out += string[0]*int(count)
+                string = string[1:]
+                count = ''
+        else:
+            break
+    return string_out
+
+
+print(rle_mtd('ABCABCABCDDDFFFFFF'))    # from wikipedia 1A1B1C1A1B1C1A1B1C3D6F
+print(rle_mtd('WWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW'))   # 9W3B24W1B14W
+print(rle_bck(rle_mtd('WWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW')))
+print(rle_bck(rle_mtd('ABCABCABCDDDFFFFFF')))
